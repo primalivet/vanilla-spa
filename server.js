ae28749a7e4ba5a508b385fbd39b54e04d7ec414
Primalivet <gustafholm1@gmail.com>
2018-04-05 13:31:55 +0200

    linted

--- server.js
+++ server.js
@@ -15,16 +15,13 @@ const Right = x => ({
   inspect: () => `Right(${x})`
 })
 
-const fromNullable = x => x === undefined || x === null ? Left(x) : Right(x)
+const fromNullable = x => (x === undefined || x === null ? Left(x) : Right(x))
 
 const apply = (x, f) => f(x)
 
 const compose = (...fs) => x => fs.reduceRight(apply, x)
 
 const prop = k => o => o[k]
-
-const parsePath = x => x.split('/').filter(y => y !== '')
-const parseQuery = x => x.split('&').map(y => y.split('='))
 
 const split = x => ys => ys.split(x)
 const filter = x => ys => ys.filter(x)
@@ -34,32 +31,34 @@ const notEmptyString = x => x !== ''
 const getPath = compose(fromNullable, prop('pathname'), url.parse)
 const getQuery = compose(fromNullable, prop('query'), url.parse)
 
-http.createServer((req, res) => {
-  // ignore favicon requests for now
-  if (req.url === '/favicon.ico') return res.end()
+http
+  .createServer((req, res) => {
+    // ignore favicon requests for now
+    if (req.url === '/favicon.ico') return res.end()
 
-  const path = getPath(req.url)
-    .map(split('/'))
-    .map(filter(notEmptyString))
-    .fold(x => x, x => x)
+    const path = getPath(req.url)
+      .map(split('/'))
+      .map(filter(notEmptyString))
+      .fold(x => x, x => x)
 
-  const query = getQuery(req.url)
-    .map(split('&'))
-    .map(x => x.map(split('=')))
-    .fold(x => x, x => x)
+    const query = getQuery(req.url)
+      .map(split('&'))
+      .map(x => x.map(split('=')))
+      .fold(x => x, x => x)
 
-  res.setHeader('Content-Type', 'application/json')
+    res.setHeader('Content-Type', 'application/json')
 
-  switch(path[0]) {
-    case 'hello':
-      res.write(JSON.stringify({ hello: 'world'}))
-      break;
-    default:
-      res.write(JSON.stringify({ you: 'leave' }))
-      break;
-  }
+    switch (path[0]) {
+      case 'hello':
+        res.write(JSON.stringify({ hello: 'world' }))
+        break
+      default:
+        res.write(JSON.stringify({ you: 'leave' }))
+        break
+    }
 
-  res.end()
-}).listen(PORT)
+    res.end()
+  })
+  .listen(PORT)
 
 console.log(`Server running on port: ${PORT}`)
